78c05f33c5c6792f7bf5679824cc4541f23884c9
gino <lorenzo.rondinelli48@gmail.com>
2023-03-22 02:07:39 +0100

    Update log.h

--- TcpProxy/log.h
+++ TcpProxy/log.h
@@ -1,5 +1,23 @@
-#pragma once
-class log
-{
-};
+#ifndef LOG_H
+#define LOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define LOG_MESSAGE_MAX_SIZE 50
+
+typedef enum {
+    INFO,
+    ERR
+} LogType;
+
+typedef struct {
+    time_t timestamp;
+    LogType type;
+    char message[LOG_MESSAGE_MAX_SIZE];
+} LogMessage;
+
+void logToFile(LogType type, char* message);
+
+#endif
