e460313f6d11ada1e51f7673efef63f5051b7afd
gino <lorenzo.rondinelli48@gmail.com>
2023-03-22 02:08:29 +0100

    Update utility.cpp

--- TcpProxy/utility.cpp
+++ TcpProxy/utility.cpp
@@ -1,7 +1,7 @@
 #include "utility.h"
-
-void set_textColor(HANDLE hConsole, char* text, char* msg) {
-
+#pragma warning(disable:4996)
+void set_textColor(HANDLE hConsole, char* text, char* msg){
+ 
     int text_color = 0;
 
     if (strcmp(msg, "err") == 0)
@@ -10,11 +10,20 @@ void set_textColor(HANDLE hConsole, char* text, char* msg) {
         text_color = GREEN;
     else if (strcmp(msg, "warn") == 0)
         text_color = YELLOW;
-
+     
     SetConsoleTextAttribute(hConsole, text_color);
     printf("%s", text);
     SetConsoleTextAttribute(hConsole, WHITE);
     printf("\n");
+    
+}
+char* get_time() {
+    
+    time_t now = time(NULL);
+    struct tm* local = localtime(&now);
+    char formatted_time[80];
+    strftime(formatted_time, sizeof(formatted_time), "%H:%M:%S", local);
 
+    return formatted_time;
 }
 
