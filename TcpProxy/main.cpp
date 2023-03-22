3ff7ac128ba3e846b065630cd47a9604c46923bb
gino <lorenzo.rondinelli48@gmail.com>
2023-03-22 02:06:21 +0100

    Update main.cpp

--- TcpProxy/main.cpp
+++ TcpProxy/main.cpp
@@ -14,56 +14,75 @@ int main(int argc, char* argv[]) {
     printf("       |_| |___|  _|__|  |_| |___|_,_|_  |\n");
     printf("               |_|                   |___|\n");
 
-    if (argc != 4) {
+   if (argc != 4) {
         fprintf(stderr, "Utilizzo: %s <local_port> <remote_host> <remote_port>\n", argv[0]);
         exit(1);
     }
-    printf("\n\n------------> Inizializzazione della librearia winsock() \n");
+    time_ = get_time();
+    printf("\n\n[%s]Creazione del file log() ...    ",time_);
+    
+    set_textColor(hConsole, (char*)"OK!", (char*)"ok");
+    time_ = get_time();
+    printf("[%s]Inizializzazione della librearia winsock() ...    ",time_);
     WSADATA wsa;
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
         error((char*)"Errore nell'inizializzazione della libreria winsock!");
     }
 
-    set_textColor(hConsole, (char*)"OK!", (char*)"ok");
-
+    set_textColor(hConsole, (char*)"OK!",(char*)"ok");
+    
+    time_ = get_time();
     // Inizializzazione del FIREWALL
-    printf("------------> Inizializzazione del firewall() \n");
+    printf("[%s]Inizializzazione del firewall() ... \n",time_);
+    printf("\nIP_BAN (LIST): \n");
     init_firewall();
     // 
     int local_port = atoi(argv[1]);
     char* remote_host = argv[2];
     int remote_port = atoi(argv[3]);
 
-    //Inizializzazione del PROXY
-    printf("------------> Inizializzazione del Proxy\n");
     int sockfd = create_socket();
-    printf("\n                   PROXY [ON] | LOCAL_PORT: %d  REMOTE_SERVER %s:%d...\n", local_port, remote_host, remote_port);
-    bind_socket(sockfd, local_port);
-    //
+    time_ = get_time();
+    printf("[%s]Inizializzazione del Proxy ...   ",time_);
+    set_textColor(hConsole, (char*)"OK!", (char*)"ok");
+
+    printf("\n                   PROXY [ON] | LOCAL_PORT: %d  REMOTE_SERVER %s:%d  ...\n", local_port, remote_host, remote_port);
+
+    bind_socket(sockfd, local_port); 
 
     struct sockaddr_in client_address;
     int client_len = sizeof(client_address);
     int server_sockfd = NULL;
-
+    char* ip_client;
 
     while (1) {
-
+        time_ = get_time();
         int client_sockfd = accept(sockfd, (struct sockaddr*)&client_address, &client_len);
-
-        //FIREWALL CONTROLLA LE CONNESSIONI IN ENTRATA
-        if (!is_allowed(client_sockfd, remote_host, remote_port)) {
-            printf("[FIREWALL]: Accesso negato al client: %s \n", client_address.sin_addr);
-            closesocket(client_sockfd);
-        }
-        //
-
+        ip_client = get_client_ip(client_sockfd);
+        
         if (client_sockfd < 0) {
             error((char*)"Error accepting connection from client");
         }
-        server_sockfd = connect_to_server(remote_host, remote_port);
-        handle_client(client_sockfd, server_sockfd);
 
-        closesocket(client_sockfd);
+        //FIREWALL CONTROLLA LE CONNESSIONI IN ENTRATA
+        if (!is_allowed(client_sockfd, remote_host, remote_port)) {
+            
+            SetConsoleTextAttribute(hConsole, YELLOW);
+            printf("[%s][FIREWALL]: Accesso negato al client: %s \n",time_, ip_client);
+            SetConsoleTextAttribute(hConsole, WHITE);
+
+            logToFile(ERR, (char*)"Acceso Negato");
+            closesocket(client_sockfd);
+        }
+        //
+        else {
+            
+            printf("\n[%s][IN]: Connessione stabilita con %s\n", time_,ip_client);
+            server_sockfd = connect_to_server(remote_host, remote_port);
+            handle_client(client_sockfd, server_sockfd);
+        }
+
+       // closesocket(client_sockfd);
     }
 
     closesocket(sockfd);
