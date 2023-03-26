e4848eaf9c09a64f6e29c0c67725a8b98ff8b962
gino <lorenzo.rondinelli48@gmail.com>
2023-03-26 22:41:51 +0200

    Update firewall.h

--- TcpProxy/firewall.h
+++ TcpProxy/firewall.h
@@ -1,33 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
 
-#ifndef FIREWALL_H
-#define FIREWALL_H
+#define MAX_IP 20
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <winsock2.h>
-#include <iphlpapi.h>
-#include "utility.h"
-#pragma comment(lib,"ws2_32.lib")
-#pragma comment(lib,"iphlpapi.lib")
-#pragma warning(disable:4996)
+class firewall {
+public:
+	firewall(std::string firewall_rules_file);
+	~firewall();
+	bool check_connection(std::string ip);
+	void shutdown_firewall();
 
-
-#define RULES_FILE "firewall.rules"
-
-// Struttura dati per le regole del firewall
-typedef struct {
-    char local_subnet[20];
-    char remote_subnet[20];
-    int remote_port;
-} firewall_rule;
-
-// Array di regole del firewall
-
-int init_firewall(); 
-void shutdown_firewall();
-int is_allowed(SOCKET client_socket, const char* remote_host, int remote_port);
-char* get_client_ip(SOCKET client_socket);
-DWORD get_subnet_mask(DWORD interface_index);
-
-#endif /* FIREWALL_H */
+private:
+	int n_ip;
+	std::string ip_ban[MAX_IP];
+};
